c7b957ff578c42c923f0c57c22e556588169aed5
Catie Hart <cmh@anniecannons.com>
2023-10-24 12:11:28 -0700

    first commit

---
+++ AirBnb/AirBnb.js
@@ -0,0 +1,18 @@
+/**
+ Using the image of the Airbnb UI, answer the following questions:
+ 1. What are four main components you see on the page?
+
+
+
+
+
+ 2. Put the components in order to show the parent/child relationship. So if component x is the mother of y and grandmother of z, you would write: 
+    1. x
+    2. y
+    3. z
+
+
+
+
+3. Which component needs access to every piece of data? Which component only needs access to the filtered data?
+ */
